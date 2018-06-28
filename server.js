16ba32f39609cbcf47411b63390673074a58e603
kamilszmajdzinski <kamil.szmajdzinski@gmail.com>
2018-06-28 23:37:09 +0200

    add api routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const items = require('./routes/api/Items')
 
 const app = express()
 
@@ -12,6 +12,8 @@ const db = require('./config/keys').mongodb
 mongoose.connect(db)
     .then(() => console.log('mongo connected'))
     .catch(err => console.log(err))
+
+app.use('/api/items', items)
 
 const port = process.env.PORT || 5000;
 
